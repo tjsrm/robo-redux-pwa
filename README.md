0935fc883f0be10448193df3092b384150e5de9e
tjsrm <71216647+tjsrm@users.noreply.github.com>
2020-10-22 14:37:01 +0530

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# robo-redux-pwa
+# robofriends
+React + Redux
+
+To run the project:
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
---
+++ src/components/CounterButton.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class CounterButton extends Component{
+    constructor(){
+        super();
+        this.state={
+            count:0
+        }
+    }
+
+
+    shouldComponentUpdate(nextProps,nextState ){
+        console.log(nextProps,nextState)
+        if(this.state.count !== nextState.count){
+            console.log(this.state.count,nextState.count)
+            return true;
+        }
+        return false;
+    }
+    updateCount=()=>{
+        this.setState(state =>{
+           return {count:this.state.count +1}
+        })
+    }
+
+    render(){
+        console.log('CounterButton');
+        return (
+            <button color={this.props.color} onClick={this.updateCount}>
+                Count :{this.state.count}
+            </button>
+        )
+    }
+
+}
+
+export default CounterButton;
